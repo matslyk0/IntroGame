9dae62ab3bca127a25da357696905aac6970d772
matslyk0 <matslyk0@gmail.com>
2024-10-06 23:43:05 +0100

    PickUp Prefab and Script Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,15 +7,26 @@ public class PlayerController : MonoBehaviour {
 
     public Vector2 moveValue;
     public float speed;
+    private int count;
+
+    private void Start() {
+        count = 0;
+    }
 
     void OnMove(InputValue value) {
         moveValue = value.Get<Vector2>();
     }
 
-    // Update is called once per frame
     void FixedUpdate() {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "PickUp") {
+            other.gameObject.SetActive(false);
+            count++;
+        }
     }
 }
